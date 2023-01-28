7e81c72d885c30a2b798813213919aa0cb53f905
Huguitis <mail@huguitisnodes.host>
2023-01-28 12:16:48 +0100

    Add variable handler

---
+++ variables.js
@@ -0,0 +1,12 @@
+const setting = require("./settings.js")
+
+module.exports = {
+  BotPrefix: setting.BotPrefix,
+  SuccessEmoji: setting.SuccessEmoji,
+  ErrorEmoji: setting.ErrorEmoji,
+  InfoEmoji: setting.InfoEmoji,
+
+  OriginShieldApiKey: "None",
+  OriginShieldApiPort: "None",
+  ActiveStatus: "No"
+}
